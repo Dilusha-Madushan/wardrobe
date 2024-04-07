a7646db4fcdc27ad59a61b89bccc00b68e3960b1
_Dillusha_ <77233788+Dilusha-Madushan@users.noreply.github.com>
2024-04-07 14:35:52 +0530

    Update config.py

--- app/config.py
+++ app/config.py
@@ -12,8 +12,7 @@ class Config:
     MONGO_PASSWORD = os.environ.get('MONGO_PASSWORD')
     MONGO_URI = f"mongodb+srv://{MONGO_USERNAME}:{MONGO_PASSWORD}@cluster0.avzbcs4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
-    FLASK_RUN_HOST = os.environ.get('FLASK_RUN_HOST', '127.0.0.1')
-    FLASK_RUN_PORT = int(os.environ.get('FLASK_RUN_PORT', '5000'))
+
 
     GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
     PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
