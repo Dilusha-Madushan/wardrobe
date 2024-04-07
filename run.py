027f7035761fe9e903e4a4f8d0bbb8d13fa30d2e
_Dillusha_ <dilushamadushan9912@gmail.com>
2024-04-07 14:05:19 +0530

    update run

--- run.py
+++ run.py
@@ -11,7 +11,5 @@ from app import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    host = app.config['FLASK_RUN_HOST']
-    port = app.config['FLASK_RUN_PORT']
-    app.run(host=host, port=port, debug=True)
+    app.run()
 
